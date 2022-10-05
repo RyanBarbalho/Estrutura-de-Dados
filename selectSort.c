9196b6f981573830d052308445a016490b9cc244
RyanBarbalho <106884891+RyanBarbalho@users.noreply.github.com>
2022-10-05 15:01:56 -0300

    Add files via upload
    funcao Select Sort

---
+++ selectSort.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+void swap(int x,int y)
+{
+        int aux1 = y;
+        x = aux1;
+        y = x;
+}
+
+void selectSort(int array[], int size)
+{
+    for(int i=1; i<size ; i++){
+        
+        for(int j= i; j>0 ; j--){
+            if( array[j]<array[j-1]){
+                swap( array[j], array[j-1]);
+            }
+
+        }
+    } 
+
+}
+
+void print(int vector[],int size)
+{
+    for (int i =0; i<size ; i++){
+        printf("%d", vector[i])
+    }
+    return;
+}
+
+int main()
+{
+    int array[8]= {5,8,7,3,1,7,2,4};
+    
+    printf("\n");
+    print(array,8);
+    selectSort(array,8);
+    print(array,8);
+    printf("\n");
+
+
+    
+    return 0;
+}
