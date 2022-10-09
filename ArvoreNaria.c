137a522167021b50bb4054e4534db8c34155e182
Ryan, aka wayan <106884891+RyanBarbalho@users.noreply.github.com>
2022-10-09 20:37:20 -0300

    Add files via upload
    arvore n-aria incompleta

---
+++ ArvoreNaria.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct nAryTree{
+    int data;
+    struct treeNode *children;
+    struct treeNode *next; // sibling
+
+}node;
+
+
+node *createNode(int value){
+    node *number = malloc(sizeof(node));
+
+    number -> data = value;
+    number -> children = NULL;
+    number -> next = NULL;
+}
+
+int main()
+{
+
+
+
+    return 0;
+}
